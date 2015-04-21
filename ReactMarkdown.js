ec8c42a01e46d616534a490e5910be2e4f0b6a69
Alejandro Sánchez <alejo90@users.noreply.github.com>
2015-04-21 15:59:46 -0600

    Support changing markdown dialect
    Markdown-js supports the Gruber (common markdown) and Maruku dialects. Maruku supports creating tables.

--- ReactMarkdown.js
+++ ReactMarkdown.js
@@ -7,21 +7,24 @@ var Markdown = React.createClass({
 
     propTypes: {
         text: React.PropTypes.string,
-        el: React.PropTypes.oneOf(Object.keys(React.DOM))
+        el: React.PropTypes.oneOf(Object.keys(React.DOM)),
+        dialect: React.PropTypes.string
     },
 
     getDefaultProps: function() {
         return {
             el: 'div',
-            text: ''
+            text: '',
+            dialect: 'Gruber'
         };
     },
 
     render: function() {
         var El = React.DOM[this.props.el];
+        var dialect = this.props.dialect;
         var props = objectAssign({}, this.props, {
             className: 'markdown',
-            dangerouslySetInnerHTML: { __html: markdown.parse(this.props.text) }
+            dangerouslySetInnerHTML: { __html: markdown.parse(this.props.text, dialect) }
         });
         return El(props);
     }
